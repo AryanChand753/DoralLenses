8f4bc3b72bfbdafa3efce1e9dc4361cda6ddcc81
Aryan Chand <aryanchand753@gmail.com>
2024-03-01 15:15:33 -0500

    Initial Commit

---
+++ dorallenses/src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTBucgIkUGTUkETQeWe0odGcAuUFHIrUo",
+  authDomain: "doral-lenses.firebaseapp.com",
+  projectId: "doral-lenses",
+  storageBucket: "doral-lenses.appspot.com",
+  messagingSenderId: "45586406260",
+  appId: "1:45586406260:web:f7355c0fdf753c0458aa21",
+  measurementId: "G-JDE6V8KXF8"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
