5d0561bf36a26b69071e4686121bdfa2e26c26c9
Aryan Chand <aryanchand753@gmail.com>
2024-03-03 12:08:09 -0500

    Create /Articles

---
+++ dorallenses/src/Articles/Articles.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import "./Articles.css"
+import Footer from '../components/Footer';
+
+const Articles = () => {
+    return (
+        <div>
+            Articels
+        </div>
+    );
+}
+
+export default Articles;
