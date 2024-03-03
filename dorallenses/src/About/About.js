06c872ccd1b3e27b1d073d4d0c6913c525332971
Aryan Chand <aryanchand753@gmail.com>
2024-03-03 12:00:33 -0500

    Create /About

---
+++ dorallenses/src/About/About.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Footer from '../components/Footer';
+import './About.css'
+
+export default function About() {
+
+    return (
+        <div>
+            <Hero2 />
+
+            <div className="about-page">
+                <h1>About</h1>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
